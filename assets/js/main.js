770c57a500fe757ab523dcb32766fe31356f21ef
naeemdeveloper <azharulislamnaeembd@gmail.com>
2025-04-12 15:18:43 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+(function ($) {
+    
+    "use strict"
+    $(document).ready(function () {
+
+        // mobile menu
+        $('#mobile-menu').meanmenu({
+            meanMenuContainer: '.mobile-menu',
+            meanScreenWidth: "991.98",
+            onePage: false
+        });
+
+        // magnific popup init
+        $(".popup-gallery").magnificPopup({
+            delegate: '.popup-img',
+            type: 'image',
+            gallery: {
+                enabled: true
+            },
+        });
+
+        $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+            type: "iframe",
+            mainClass: "mfp-fade",
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+    
+
+    });
+
+    $(window).on('scroll', function () {
+
+        // back to top scroll
+        var ScrollTop = $('.back-to-top');
+
+        if ($(window).scrollTop() > 1000) {
+            ScrollTop.fadeIn(1000);
+        } else {
+            ScrollTop.fadeOut(1000);
+        }
+
+    });
+
+    $(window).on('load', function () {
+
+        // wow js
+        new WOW().init();
+
+        // preloader
+        var preLoder = $("#preloader");
+        preLoder.fadeOut(0);
+
+        // back to top animate
+        $(".back-to-top").on('click', function () {
+            $("html").animate({
+                "scrollTop": "0"
+            }, 1000);
+        });
+
+    });
+
+})(jQuery);
